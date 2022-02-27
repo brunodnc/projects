c02a31813a9e7775d0bb06ad57bdb59ff22b9eac
brunodnc <brunocecchini@hotmail.com>
2022-02-27 17:16:26 -0300

    primeiro exercicio js fcc

--- trybe/1-fundamentos/bloco4-introjs/exercicio1.js
+++ trybe/1-fundamentos/bloco4-introjs/exercicio1.js
@@ -13,3 +13,34 @@ function redundantfunction(coisa) {
 
 redundantfunction(bdecade);
 redundantfunction(3);
+
+// Exercise if else and logical operators
+const nota = 90;
+const cota = true
+if (nota > 59 || (nota > 49 && cota)) {
+    console.log("Aprovada");
+} else if (nota > 39) {
+    console.log("Lista de Espera")
+} else {
+    console.log("Reprovada");
+}
+
+// Basic Exercise
+
+function Tabelinha(n) {
+    if (typeof n == "number") {
+        let soma = n + n;
+        let sub = n - n;
+        let mult = n * n;
+        let div = n / n;
+        return {"Soma": soma,
+                "Subtração": sub,
+                "Multiplicação": mult,
+                "Divisão": div,
+            }
+    } else {
+        return undefined
+    }
+}
+
+console.log(Tabelinha(5));
