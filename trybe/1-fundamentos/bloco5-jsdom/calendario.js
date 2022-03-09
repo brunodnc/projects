f214e9deddaa120a11a56a8cab2d22cf1fd8f951
brunodnc <brunocecchini@hotmail.com>
2022-03-09 19:33:27 -0300

    add buggy zoomin zoomout

--- trybe/1-fundamentos/bloco5-jsdom/calendario.js
+++ trybe/1-fundamentos/bloco5-jsdom/calendario.js
@@ -32,7 +32,10 @@ function MonthDays() {
     let liitem = document.createElement("li");
     liitem.innerText = day;
     liitem.classList.add("day");
-    if (day === 24 || day === 25 || day === 31) {
+    if (day === 25) {
+        liitem.classList.add("holiday");
+        liitem.classList.add("friday");
+    } else if (day === 24 || day === 25 || day === 31) {
       liitem.classList.add("holiday");
     } else if (day === 4 || day === 11 || day === 18 || day === 25) {
       liitem.classList.add("friday");
@@ -66,6 +69,9 @@ function bgcchange(event) {
     }
     on = false;
   }
+  if (on2 === true) {
+      bgcchange2();
+  }
 }
 
 document
@@ -82,6 +88,7 @@ document
 inputToButton2("Sexta-feira");
 
 let on2 = false;
+
 function bgcchange2(event) {
     let arrfri = document.querySelectorAll(".friday");
     if (on2 === false) {
@@ -95,8 +102,57 @@ function bgcchange2(event) {
       }
       on2 = false;
     }
+
+    if (on === true) {
+        bgcchange();
+    }
   }
 
 document
   .getElementsByClassName("btn-friday")[0]
   .addEventListener("click", bgcchange2);
+
+
+let arrdays = document
+  .getElementsByClassName('day');
+
+
+  function logtype(event) {
+      console.log(event.target);
+  }
+
+  function zoomin(event) {
+      event.target.style.fontSize = "2rem";  
+    //   COMO MUDAR TAMANHO SEM FAZER A TABELA BALANÇAR TODA????
+  }
+
+  function zoomout(event) {
+    event.target.style.fontSize = "1rem";
+}
+  for (let day of arrdays) {
+      day.addEventListener("click", logtype);
+      day.addEventListener('mouseenter', zoomin);
+      day.addEventListener('mouseleave', zoomout);
+    }
+
+
+
+//   Exercício 6:
+// Exercício 7:
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+// Exercício 8:
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+// Exercício 9:
+// Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
+// Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+// Exercício 10:
+// Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+// Bônus:
+// Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+// Se nenhum caractere for inserido no campo input , a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+// Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+// Dica - Propriedade: key .
